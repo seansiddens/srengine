1c07d17fff65d9b0731d2c63394e37a23c36b921
Sean Siddens <seansiddens@gmail.com>
2023-10-16 20:47:26 -0700

    Track file and line number of log messages

--- log.h
+++ log.h
@@ -4,41 +4,46 @@
 #include <memory>
 #include <string>
 
-#define LOG_INFO(format, ...) sren::LogService::log_info(format, ##__VA_ARGS__);
-#define LOG_ERR(format, ...) sren::LogService::log_error(format, ##__VA_ARGS__);
-#define LOG_DBG(format, ...) sren::LogService::log_debug(format, ##__VA_ARGS__);
+#define LOG_INFO(format, ...)                                                                 \
+    sren::LogService::log_info(__FILE__, __LINE__, format, ##__VA_ARGS__);
+#define LOG_ERR(format, ...)                                                                  \
+    sren::LogService::log_error(__FILE__, __LINE__, format, ##__VA_ARGS__);
+#define LOG_DBG(format, ...)                                                                  \
+    sren::LogService::log_debug(__FILE__, __LINE__, format, ##__VA_ARGS__);
 
 namespace sren {
 
 class LogService {
-public:
+  public:
     template <typename... Args>
-    static void log_info(const char* format, Args... args) {
-        log("INFO", format, args...);
+    static void log_info(const char *file, int line, const char *format, Args... args) {
+        log("INFO", file, line, format, args...);
     }
 
     template <typename... Args>
-    static void log_error(const char* format, Args... args) {
-        log("ERROR", format, args...);
+    static void log_error(const char *file, int line, const char *format, Args... args) {
+        log("ERROR", file, line, format, args...);
     }
 
     template <typename... Args>
-    static void log_debug(const char* format, Args... args) {
-        log("DEBUG", format, args...);
+    static void log_debug(const char *file, int line, const char *format, Args... args) {
+        log("DEBUG", file, line, format, args...);
     }
 
-private:
+  private:
     template <typename... Args>
-    static void log(const char* level, const char* format, Args... args) {
+    static void log(const char *level, const char *file, int line, const char *format,
+                    Args... args) {
         std::string message = string_format(format, args...);
-        std::cout << "[" << level << "]: " << message << std::endl;
+        std::cout << "[" << level << "][" << file << ":" << line << "]: " << message
+                  << std::endl;
     }
 
-    template<typename ... Args>
-    static std::string string_format(const char* format, Args ... args) {
-        size_t size = snprintf(nullptr, 0, format, args ...) + 1; // +1 for '\0'
+    template <typename... Args>
+    static std::string string_format(const char *format, Args... args) {
+        size_t size = snprintf(nullptr, 0, format, args...) + 1; // +1 for '\0'
         std::unique_ptr<char[]> buf(new char[size]);
-        snprintf(buf.get(), size, format, args ...);
+        snprintf(buf.get(), size, format, args...);
         return std::string(buf.get(), buf.get() + size - 1); // -1 for '\0'
     }
 };
