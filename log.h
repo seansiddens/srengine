2660f558e8b608abdc2360ca4198c13d5ac35171
Sean Siddens <seansiddens@gmail.com>
2023-10-16 20:42:54 -0700

    Use varadiac templates for string formatting of log messages

--- log.h
+++ log.h
@@ -1,33 +1,46 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 #include <string>
 
-#define LOG_INFO(msg) sren::LogService::log_info(msg);
-#define LOG_ERR(msg) sren::LogService::log_error(msg);
-#define LOG_DBG(msg) sren::LogService::log_debug(msg);
+#define LOG_INFO(format, ...) sren::LogService::log_info(format, ##__VA_ARGS__);
+#define LOG_ERR(format, ...) sren::LogService::log_error(format, ##__VA_ARGS__);
+#define LOG_DBG(format, ...) sren::LogService::log_debug(format, ##__VA_ARGS__);
 
 namespace sren {
 
 class LogService {
 public:
-    static void log_info(const std::string& message) {
-        log("INFO", message);
+    template <typename... Args>
+    static void log_info(const char* format, Args... args) {
+        log("INFO", format, args...);
     }
 
-    static void log_error(const std::string& message) {
-        log("ERROR", message);
+    template <typename... Args>
+    static void log_error(const char* format, Args... args) {
+        log("ERROR", format, args...);
     }
 
-    static void log_debug(const std::string& message) {
-        log("DEBUG", message);
+    template <typename... Args>
+    static void log_debug(const char* format, Args... args) {
+        log("DEBUG", format, args...);
     }
 
 private:
-    static void log(const std::string& level, const std::string& message) {
+    template <typename... Args>
+    static void log(const char* level, const char* format, Args... args) {
+        std::string message = string_format(format, args...);
         std::cout << "[" << level << "]: " << message << std::endl;
     }
-};
 
+    template<typename ... Args>
+    static std::string string_format(const char* format, Args ... args) {
+        size_t size = snprintf(nullptr, 0, format, args ...) + 1; // +1 for '\0'
+        std::unique_ptr<char[]> buf(new char[size]);
+        snprintf(buf.get(), size, format, args ...);
+        return std::string(buf.get(), buf.get() + size - 1); // -1 for '\0'
+    }
+};
 
 } // namespace sren
